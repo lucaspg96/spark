5bfcb4f7ca43d67f7c172f3bf66d25b102ad1400
Lucas Peres <lucasperes@Joaos-Mac-mini.local>
2017-05-23 14:51:40 -0300

    Silenciando INFOs do Log

--- src/main/scala/SampleApp.scala
+++ src/main/scala/SampleApp.scala
@@ -2,9 +2,12 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+
+import org.apache.log4j.{Logger, Level}
  
 object SampleApp {
   def main(args: Array[String]) {
+    Logger.getLogger("org").setLevel(Level.OFF)
     val txtFile = "file:///Users/lucasperes/Documents/Repositories/spark/src/main/scala/SampleApp.scala"
     val conf = new SparkConf().setAppName("Sample Application").setMaster("local[*]")
     val sc = new SparkContext(conf)
