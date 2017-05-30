d9fa70d490fa1339fbe631b2d09bc6956e26f521
Lucas Peres <lucasperes@Mac-mini.local>
2017-05-30 12:31:24 -0300

    Criando .gitignore

--- src/main/scala/SampleApp.scala
+++ src/main/scala/SampleApp.scala
@@ -8,12 +8,15 @@ import org.apache.log4j.{Logger, Level}
 object SampleApp {
   def main(args: Array[String]) {
     Logger.getLogger("org").setLevel(Level.OFF)
-    val txtFile = "file:///Users/lucasperes/Documents/Repositories/spark/src/main/scala/SampleApp.scala"
+    //val txtFile = "file:///Users/lucasperes/Documents/Repositories/spark/src/main/scala/SampleApp.scala"
+    val txtFile = "hdfs://localhost:54310/hdfs/SampleApp.scala"
     val conf = new SparkConf().setAppName("Sample Application").setMaster("local[*]")
     val sc = new SparkContext(conf)
     println("Ambiente iniciado")
-    val txtFileLines = sc.textFile(txtFile).cache()
+    val txtFileLines = sc.textFile(txtFile)
+    println("Arquivo guardado")
     val numAs = txtFileLines.filter(line => line.contains("val")).count()
+    println("Contagem feita")
     println("Lines with val: %s".format(numAs))
   }
 }
